44e46e11b96f538083f6b049769ef24ea2c8f8bb
Drollie123 <152189626+Drollie123@users.noreply.github.com>
2024-01-09 13:39:38 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,9 @@
+Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Red, Kitronik_STOPbit.DisplayLights.On)
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Green, Kitronik_STOPbit.DisplayLights.On)
+        basic.pause(5000)
+    } else {
+        Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Red, Kitronik_STOPbit.DisplayLights.On)
+    }
+})
